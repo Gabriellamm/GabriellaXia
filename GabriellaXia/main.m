f208a504079d68be5f6fb1fc8c6b023a086ddfc0
Gabriellamm <Gabriella@gabriellademacbook-pro.local>
2016-10-18 13:55:51 +0800

    Initial Commit

---
+++ GabriellaXia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GabriellaXia
+//
+//  Created by Gabriella on 2016/10/18.
+//  Copyright © 2016年 Gabriella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
